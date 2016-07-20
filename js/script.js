aa90e510f82d4b461789e0ed8ed5a005a5f4aaf8
Peter Springer <peterspringer829@gmail.com>
2016-07-20 16:33:23 -0600

    get ideas from storage and append them

--- js/script.js
+++ js/script.js
@@ -1,28 +1,61 @@
 var userInputTitle = $('.input-title');
 var userInputBody = $('.input-body');
+var searchValue = $('.search-input');
 var allIdeas = [];
 
-function getIdeas() {
-}
-
-function Idea (title, body){
+function Idea (title, body, quality){
   this.title = title;
   this.body = body;
+  this.quality = quality;
   this.id = Date.now();
 }
 
+function deleteIdeas() {
+  $('.delete').on('click', function(){
+    $(this).parent().remove();
+  });
+}
+
+Idea.prototype.storeIdea = function () {
+  localStorage.setItem('ideas', JSON.stringify(allIdeas));
+}
+
+function returnIdeas() {
+  return JSON.parse(localStorage.getItem('ideas'));
+}
+//
+// function getIdeasFromStorageAndAppendThem() {
+//   var ideas = returnIdeas();
+//   for (var idea of ideas) {
+//     createOutput(idea.title, idea.body);
+//   }
+//   deleteIdeas()
+// }
+
+
+function getIdeasFromStorageAndAppendThem() {
+  var ideas = returnIdeas();
+  for (var i = 0; i < ideas.length; i++) {
+    var idea = ideas[i]
+    createOutput(idea.title, idea.body);
+  }
+}
+// Block.prototype.moveRight = function () {
+//   this.x = this.x + 1;
+//   return this;
+
 function createOutput(title, body) {
-$('.all-ideas').prepend(
-  `<li class="user-idea">
-    <h3 class="user-idea-title">${title}</h3>
-    <button class="idea-button delete" type="button"></button>
-    <p class="user-idea-body">${body}</p>
-    <footer class="idea-footer">
-    <button class="idea-button upvote" type="button"></button>
-    <button class="idea-button downvote" type="button"></button>
-    <p class="ranking">quality: swill</p>
-    </footer>
-   </li>`
+  $('.all-ideas').prepend(
+    `<li class="user-idea">
+      <h3 class="user-idea-title">${title}</h3>
+      <button class="idea-button delete" type="button"></button>
+      <p class="user-idea-body">${body}</p>
+      <footer class="idea-footer">
+      <button class="idea-button upvote" type="button"></button>
+      <button class="idea-button downvote" type="button"></button>
+      <p class="quality">quality: swill</p>
+      </footer>
+     </li>`
   );
 };
 
@@ -31,8 +64,14 @@ $('.save-button').on('click', function(event) {
   var title = userInputTitle.val();
   var body = userInputBody.val();
   var buildNewIdea = new Idea(title, body);
-  allIdeas.push(buildNewIdea);
   var userIdeas = createOutput(title, body);
-  localStorage.setItem('ideas', JSON.stringify(allIdeas));
-  JSON.parse(localStorage.getItem('ideas'));
+  allIdeas.push(buildNewIdea);
+  buildNewIdea.storeIdea();
+  deleteIdeas()
 });
+
+getIdeasFromStorageAndAppendThem()
+// $('.search-input').on('keyup', function() {
+//   var searchedValue = $('.layout-section-user-output').has('.user-idea-title').val(searchValue.val());
+//   // allIdeas.title = searchedValue;
+// });
