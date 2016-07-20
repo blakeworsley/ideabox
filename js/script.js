6381e7a332dc77ae8ea50a5382775a4be05d1c9b
Peter Springer <peterspringer829@gmail.com>
2016-07-19 20:21:41 -0600

    refactored save button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var ideas = [];
+var userInputTitle = $('.input-title');
+var userInputBody = $('.input-body');
 
 function Idea (title, body, rank){
   this.title = title;
@@ -7,21 +9,26 @@ function Idea (title, body, rank){
   this.rank = rank;
 }
 
+function createOutput(title, body) {
+$('.all-ideas').prepend(
+  `<li class="user-idea">
+    <h3 class="user-idea-title">${title}</h3>
+    <button class="id-button delete" type="button"></button>
+    <p class="user-idea-body">${body}</p>
+    <footer class="idea-footer">
+    <button class="id-button upvote" type="button"></button>
+    <button class="id-button downvote" type="button"></button>
+    <p class="ranking">quality: swill</p>
+    </footer>
+   </li>`
+  );
+};
 
 $('.save-button').on('click', function(event) {
   event.preventDefault();
-  var userInputTitle = $('.input-title').val();
-  var userInputBody = $('.input-body').val();
+  var title = userInputTitle.val();
+  var body = userInputBody.val();
   var buildNewIdea = new Idea(userInputTitle, userInputBody);
   ideas.push(buildNewIdea);
-  $('.all-ideas').prepend(
-  '<li class="user-idea">' +
-    '<h3 class="user-idea-title">' + userInputTitle + '</h3>' +
-    '<img src="img/delete.svg" class="idea-button delete">' +
-    '<p class="user-idea-body">' + userInputBody + '</p>' +
-    '<img src="img/upvote.svg" class="idea-button upvote">' +
-    '<img src="img/downvote.svg" class="idea-button downvote">' +
-    '<p class="ranking">ranking: swill</p>' +
-  '</li>'
-  );
+  var userIdeas = createOutput(title, body);
 });
