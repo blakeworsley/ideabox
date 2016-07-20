dfc9f8e304b5b5967d01a2333dadd7046e5d1b8c
Peter Springer <peterspringer829@gmail.com>
2016-07-20 08:49:35 -0600

    first pass at setting and getting local storage

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-var ideas = [];
 var userInputTitle = $('.input-title');
 var userInputBody = $('.input-body');
+var allIdeas = [];
 
-function Idea (title, body, rank){
+function getIdeas() {
+}
+
+function Idea (title, body){
   this.title = title;
   this.body = body;
   this.id = Date.now();
-  this.rank = rank;
 }
 
 function createOutput(title, body) {
 $('.all-ideas').prepend(
   `<li class="user-idea">
     <h3 class="user-idea-title">${title}</h3>
-    <button class="id-button delete" type="button"></button>
+    <button class="idea-button delete" type="button"></button>
     <p class="user-idea-body">${body}</p>
     <footer class="idea-footer">
-    <button class="id-button upvote" type="button"></button>
-    <button class="id-button downvote" type="button"></button>
+    <button class="idea-button upvote" type="button"></button>
+    <button class="idea-button downvote" type="button"></button>
     <p class="ranking">quality: swill</p>
     </footer>
    </li>`
@@ -28,7 +30,9 @@ $('.save-button').on('click', function(event) {
   event.preventDefault();
   var title = userInputTitle.val();
   var body = userInputBody.val();
-  var buildNewIdea = new Idea(userInputTitle, userInputBody);
-  ideas.push(buildNewIdea);
+  var buildNewIdea = new Idea(title, body);
+  allIdeas.push(buildNewIdea);
   var userIdeas = createOutput(title, body);
+  localStorage.setItem('ideas', JSON.stringify(allIdeas));
+  JSON.parse(localStorage.getItem('ideas'));
 });
