652d3180c1aabd6533a762d92518669fe8b78c21
Blake Worsley <blakeworsley@gmail.com>
2016-07-21 06:51:00 -0600

    upvote button function created with variable defined in the upvoteIdeas function -Still need to play with the funcitonality

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ var userInputTitle = $('.input-title');
 var userInputBody = $('.input-body');
 var searchValue = $('.search-input');
 var allIdeas = [];
+var qualities = ['swill', 'plausible', 'genius'];
+//                  0           1          2
+
+//push adds to array. push removes from array
+
 
 function Idea (title, body, quality){
   this.title = title;
@@ -16,9 +21,49 @@ function deleteIdeas() {
   });
 }
 
+//TODO: Disable Downvote function disableDownvote()
+
+// function currentI() {
+//   if (this.quality === "swill"){
+//     return 0;
+//   }
+//   if (this.quality === "plausible"){
+//     return 1;
+//   }
+//   if (this.quality === "genius"){
+//     return 2;
+//   }
+// }
+
+// var i = currentI()
+function currentQuality(i) {
+  for ( i; i < qualities.length; i++) {
+    if (i === 2) {
+      return 'genius';
+    }
+    if (i === 1) {
+      i++;
+      return 'genius';
+    }
+    if (i === 0) {
+      i++;
+      return 'plausible';
+    }
+  }
+}
+
+function upvoteIdeas() {
+  var i = 0
+  $('.upvote').on('click', function(){
+    $(this).siblings('.quality').text('quality: ' + currentQuality(i));
+    //select the .quality and change quality to
+  });
+}
+
+//TODO: if you add new idea to current stored ideas / stored ideas are deleted
 Idea.prototype.storeIdea = function () {
   localStorage.setItem('ideas', JSON.stringify(allIdeas));
-}
+};
 
 function returnIdeas() {
   return JSON.parse(localStorage.getItem('ideas'));
@@ -36,9 +81,11 @@ function returnIdeas() {
 function getIdeasFromStorageAndAppendThem() {
   var ideas = returnIdeas();
   for (var i = 0; i < ideas.length; i++) {
-    var idea = ideas[i]
+    var idea = ideas[i];
     createOutput(idea.title, idea.body);
   }
+  deleteIdeas();
+  upvoteIdeas();
 }
 // Block.prototype.moveRight = function () {
 //   this.x = this.x + 1;
@@ -53,11 +100,11 @@ function createOutput(title, body) {
       <footer class="idea-footer">
       <button class="idea-button upvote" type="button"></button>
       <button class="idea-button downvote" type="button"></button>
-      <p class="quality">quality: swill</p>
+      <p class="quality">quality: ${qualities[0]}</p>
       </footer>
      </li>`
   );
-};
+}
 
 $('.save-button').on('click', function(event) {
   event.preventDefault();
@@ -67,10 +114,11 @@ $('.save-button').on('click', function(event) {
   var userIdeas = createOutput(title, body);
   allIdeas.push(buildNewIdea);
   buildNewIdea.storeIdea();
-  deleteIdeas()
+  deleteIdeas();
+  upvoteIdeas();
 });
 
-getIdeasFromStorageAndAppendThem()
+getIdeasFromStorageAndAppendThem();
 // $('.search-input').on('keyup', function() {
 //   var searchedValue = $('.layout-section-user-output').has('.user-idea-title').val(searchValue.val());
 //   // allIdeas.title = searchedValue;
